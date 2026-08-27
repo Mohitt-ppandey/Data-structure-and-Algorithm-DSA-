55fc53e06ef8ba33012483f26d61207fe30046e2
Mohit pandey <166816931+Mohitt-ppandey@users.noreply.github.com>
2026-08-27 23:54:16 +0530

    Time: 1 ms (50.75%) | Memory: 43.5 MB (65.50%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> letterCombinations(String str) {
+        HashMap<Character , List<Character>> map = new HashMap<>();
+        char a = 'a';
+        for (char ch='2'; ch<='9'; ch++){
+            for (int i=1; i<=3; i++){
+                if (!map.containsKey(ch)) map.put(ch , new ArrayList<>());
+                map.get(ch).add(a++);
+            }
+            if (ch == '7' || ch == '9') map.get(ch).add(a++);
+        }
+        List<String> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        helper(str ,  sb, map , ans , 0);
+        return ans;
+    }
+
+    public void helper(String str, StringBuilder s, HashMap<Character, List<Character>> map , List<String> ans , int idx) {
+        if (idx == str.length()) {
+            ans.add(s.toString());
+            return;
+        }
+        for (int i=0; i<map.get(str.charAt(idx)).size(); i++) {
+            helper(str, s.append(map.get(str.charAt(idx)).get(i)) , map, ans, idx + 1);
+            s.deleteCharAt(s.length()-1);
+        }
+    }
+}
