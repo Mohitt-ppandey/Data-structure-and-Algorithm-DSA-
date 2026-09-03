1180c3d37161d98ff4f3cd466a085fa28b3c3e2b
Mohit pandey <166816931+Mohitt-ppandey@users.noreply.github.com>
2026-09-03 11:17:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (81.17%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,17 +3,14 @@ class Solution {
         int m = arr.length , n = arr[0].length;
         int[][] dp = new int[m][n];
         if(arr[0][0] == 1) return 0;
+        if(arr[m-1][n-1] == 1) return 0;
         for(int i=0; i<m; i++) {
             if(arr[i][0] == 0) dp[i][0] = 1;
-            else {
-                break;
-            }
+            else break;
         }
         for(int j=0; j<n; j++) {
             if(arr[0][j] == 0) dp[0][j] = 1;
-            else {
-                break;
-            }
+            else break;
         }
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
