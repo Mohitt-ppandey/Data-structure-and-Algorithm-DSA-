a633b79e8848f5eb881a7fb19e3f38bf4e7f12b8
Mohit pandey <166816931+Mohitt-ppandey@users.noreply.github.com>
2026-09-04 14:40:54 +0530

    Time: 4 ms (60.49%) | Memory: 50.1 MB (85.27%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,18 +1,15 @@
 class Solution {
-    static int[][] dp;
     public int minPathSum(int[][] arr) {
-        int n = arr.length , m = arr[0].length;
-        dp = new int[n][m];
-        for(int i=0; i<n; i++) Arrays.fill(dp[i] , -1);
-        return minSum(arr , 0 , 0);
-    }
-    public int minSum(int[][] arr , int i , int j){
-        if(i == arr.length-1 && j == arr[0].length-1) return arr[i][j];
-        if(i == arr.length-1) return arr[i][j] + minSum(arr , i , j+1);
-        if(j == arr[0].length-1) return arr[i][j] + minSum(arr , i+1 , j);
-        if(dp[i][j] != -1) return dp[i][j];
-        int a = arr[i][j] + minSum(arr , i+1 , j);
-        int b = arr[i][j] + minSum(arr , i , j+1);
-        return dp[i][j] = Math.min(a , b);
+        int m = arr.length , n = arr[0].length; 
+        int[][] dp = new int[m][n];
+        dp[0][0] = arr[0][0];
+        for(int i=1; i<m; i++) dp[i][0] = arr[i][0] + dp[i-1][0];
+        for(int j=1; j<n; j++) dp[0][j] = arr[0][j] + dp[0][j-1];
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++){
+                dp[i][j] = arr[i][j] + Math.min(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
