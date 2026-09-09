57ea047e61097078e58de19225945c5af1651ffc
Mohit pandey <166816931+Mohitt-ppandey@users.noreply.github.com>
2026-09-09 11:49:13 +0530

    Time: 2 ms (68.79%) | Memory: 45 MB (93.55%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,18 +1,16 @@
 class Solution {
-    static int[][] dp;
     public static int calculateMinimumHP(int[][] arr) {
         int m = arr.length , n = arr[0].length;
-        dp = new int[m][n];
-        for(int i=0; i<m; i++) Arrays.fill(dp[i] , -1);
-        return fxn(arr , 0 , 0);
-    }
-    public static int fxn(int[][] arr , int i , int j) {
-        if (i >= arr.length || j >= arr[0].length) return Integer.MAX_VALUE;
-        if (i == arr.length-1 && j == arr[0].length-1) return arr[i][j] >= 0 ? 1 : Math.abs(arr[i][j])+1;
-        if(dp[i][j] != -1) return dp[i][j];
-        int r = fxn(arr , i , j+1);
-        int d = fxn(arr , i+1 , j);
-        int res = arr[i][j] - Math.min(r , d);
-        return dp[i][j] = res >=0 ? 1 : Math.abs(res);
+        int[][] dp = new int[m][n];
+        dp[m-1][n-1] = (arr[m-1][n-1] >=  0) ? 1 : Math.abs(arr[m-1][n-1])+1;
+        for (int i=m-2; i>=0; i--) dp[i][n-1] = ((arr[i][n-1]-dp[i+1][n-1]) >=  0) ? 1 : dp[i+1][n-1] - arr[i][n-1];
+        for (int j=n-2; j>=0; j--)dp[m-1][j] = ((arr[m-1][j]-dp[m-1][j+1]) >=  0) ? 1 : dp[m-1][j+1] - arr[m-1][j];
+        for(int i=m-2; i>=0; i--){
+            for (int j=n-2; j>=0; j--){
+                int min = Math.min(dp[i+1][j] , dp[i][j+1]);
+                dp[i][j] =  ((arr[i][j] - min) >= 0) ? 1 : min - arr[i][j];
+            }
+        }
+        return dp[0][0];
     }
 }
