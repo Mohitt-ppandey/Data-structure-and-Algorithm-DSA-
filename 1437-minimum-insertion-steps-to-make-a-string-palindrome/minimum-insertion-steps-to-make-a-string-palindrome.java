ac2defbe780b1755fbc47bd7aae3ae93e2c0cc3f
Mohit pandey <166816931+Mohitt-ppandey@users.noreply.github.com>
2026-09-10 16:44:54 +0530

    Time: 19 ms (84.69%) | Memory: 47.8 MB (11.19%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -7,9 +7,9 @@ class Solution {
         return n-lcp(s , 0 , n-1);
     }
     public int lcp(String s , int i , int j){
+        if(i == j) return 1;
         if(i > j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        if(i == j) return dp[i][j] = 1;
         else if(s.charAt(i) == s.charAt(j)) return dp[i][j] = 2 + lcp(s , i+1 , j-1);
         else return  dp[i][j] = Math.max(lcp(s , i+1 , j) , lcp(s , i , j-1));
     }
