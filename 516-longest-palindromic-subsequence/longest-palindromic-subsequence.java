0a17f189182376bcded80505ef3659213356a22d
Mohit pandey <166816931+Mohitt-ppandey@users.noreply.github.com>
2026-09-10 15:47:44 +0530

    Time: 29 ms (81.92%) | Memory: 88.6 MB (6.14%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,16 +1,17 @@
 class Solution {
     static int[][] dp;
     public int longestPalindromeSubseq(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
         int m = s.length();
+        if(m==1) return 1;
         dp = new int[m][m];
         for(int[] ele : dp) Arrays.fill(ele,-1);
-        return LCS(s , rev , m-1 , m-1);
+        return LCS(s , 0 , m-1);
     }
-    public int LCS(String a, String b , int i , int j){
-        if(i < 0 || j < 0) return 0;
+    public int LCS(String a , int i , int j){
+        if(i > j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        if(a.charAt(i) == b.charAt(j)) return dp[i][j] = 1 + LCS(a , b , i-1 , j-1);
-        else return dp[i][j] = Math.max(LCS(a , b , i-1 , j) , LCS(a , b , i , j-1));
+        if(i == j) return dp[i][j] = 1 + LCS(a , i+1 , j-1);
+        else if(a.charAt(i) == a.charAt(j)) return dp[i][j] = 2 + LCS(a , i+1 , j-1);
+        else return dp[i][j] = Math.max(LCS(a , i+1 , j) , LCS(a , i , j-1));
     }
 }
