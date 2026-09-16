f6a226e23b4356aa4eb10b6ad12cc3329ff30607
Mohit pandey <166816931+Mohitt-ppandey@users.noreply.github.com>
2026-09-16 22:23:18 +0530

    Time: 6 ms (17.41%) | Memory: 43.4 MB (47.26%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.java
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.java
@@ -1,20 +1,20 @@
 class Solution {
-    static int totalSum;
     static int[][] dp;
     public int lastStoneWeightII(int[] arr) {
-        totalSum = 0;
+        int n = arr.length;
+        int totalSum = 0;
         for(int ele : arr) totalSum += ele;
-        dp = new int[arr.length][totalSum];
-        for(int[] ele : dp) Arrays.fill(ele , -1); 
-        int a = fxn(arr , 0 , 0);
-        return Math.abs(2*a - totalSum);
-    }
-    public int fxn(int[] arr , int idx , int sum){
-        if(idx == arr.length) return 0;
-        if(dp[idx][sum] != -1) return dp[idx][sum];
-        int skip = fxn(arr , idx+1 , sum);
-        int pick = Integer.MIN_VALUE;
-        if(sum+arr[idx] <= totalSum/2)  pick = arr[idx] + fxn(arr , idx+1 , sum+arr[idx]);
-        return dp[idx][sum] = Math.max(pick , skip);
+        int sum = totalSum/2;
+        dp = new int[n][sum+1];
+        if(arr[0] <= sum) for(int j=arr[0]; j<=sum; j++) dp[0][j] = arr[0];
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=sum; j++){
+                int skip = dp[i-1][j];
+                int pick = Integer.MIN_VALUE;
+                if(arr[i] <= j) pick =arr[i] + dp[i-1][j-arr[i]];
+                dp[i][j] = Math.max(skip , pick);
+            }
+        }
+        return  Math.abs(totalSum - 2 * dp[n-1][sum]);
     }
 }
