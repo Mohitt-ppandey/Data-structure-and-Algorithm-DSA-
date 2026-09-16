842b2974a579e3d3bc97324111fd5f42c7d4a342
Mohit pandey <166816931+Mohitt-ppandey@users.noreply.github.com>
2026-09-17 00:11:12 +0530

    Time: 106 ms (38.15%) | Memory: 47.5 MB (5.49%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,15 +1,18 @@
 class Solution {
     static int Target;
+    static HashMap<String , Integer> dp;
     public int findTargetSumWays(int[] arr, int target) {
         Target = target;
+        dp = new HashMap<>();
         return sum(arr , 0 , 0);
     }
     public int sum(int[] arr , int idx , int sum){
-        if(idx >= arr.length){
-            return (sum == Target) ? 1 : 0;
-        }
+        if(idx >= arr.length) return (sum == Target) ? 1 : 0;
+        String key = sum + " , " + idx;
+        if(dp.containsKey(key)) return dp.get(key);
         int a = sum(arr , idx+1 , sum+arr[idx]);
         int b = sum(arr , idx+1 , sum-arr[idx]);
+        dp.put(key , a+b);
         return a+b;
     }
 }
