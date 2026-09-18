f1d3cdf5563cbb5ef47d355b9c92fa0b2f84a135
Mohit pandey <166816931+Mohitt-ppandey@users.noreply.github.com>
2026-09-19 01:08:12 +0530

    Time: 2884 ms (5.20%) | Memory: 48.7 MB (41.89%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int mod = (int) 1e9+7;
+    static int[][] dp;
+    public int numberOfSets(int n, int k) {
+        if((n-k) <=1) return 1;
+        if(k == 1) return (int)(n*(n-1)/2);
+        dp = new int[n][k+1];
+        for(int[] el : dp) Arrays.fill(el , -1);
+        return fxn(0 , k , n);
+    }
+    public int fxn(int idx , int k , int n){
+        if(k == 0) return 1;
+        if(idx >= n) return 0;
+        if(dp[idx][k] != -1) return dp[idx][k];
+        int skip = fxn(idx+1 , k , n);
+        int pick = 0;
+        for(int i=idx+1; i<n; i++) pick = (pick + fxn(i , k-1 , n))%mod;
+        return dp[idx][k] = (pick + skip)%mod;
+    }
+}
